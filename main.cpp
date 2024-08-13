ff6a13d8e4cd29060b4dbebecba3f099736ff7d7
kakaintest <168247648+kakaintest@users.noreply.github.com>
2024-08-13 23:11:08 +0700

    Submit code at 00:10:15 14/8/2024 by HiCommit

--- main.cpp
+++ main.cpp
@@ -1,15 +1,10 @@
-// HICOMMIT TEMPLATE FOR C++
 #include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    /*
-        Your code goes here. Happy coding!
-    */
-
+    cin.tie(NULL)
+    
     return 0;
 }
