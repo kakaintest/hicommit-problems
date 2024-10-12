7345d70fe194bdd67c1a65360dd79b741a1f0b9d
kakaintest <168247648+kakaintest@users.noreply.github.com>
2024-10-12 20:37:51 +0700

    Submit code at 20:35:57 12/10/2024 by HiCommit

--- main.cpp
+++ main.cpp
@@ -1,15 +1,38 @@
-// HICOMMIT TEMPLATE FOR C++
-#include <bits/stdc++.h>
+#include <stdio.h>
+//#include <math.h>
+#define MAXN 10005
+int n, h[MAXN], f[MAXN];
 
-using namespace std;
+int min(int a,int b);
+int abs(int a);
+int chiphi(int i);
+
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+	scanf("%d",&n);
+	for(int i = 1; i <= n; i++) scanf("%d",&h[i]);
+		for(int i = 1; i <= n; i++) f[i]=-1;
+	printf("%d",chiphi(1));
+	return 0;
+}
 
-    /*
-        Your code goes here. Happy coding!
-    */
+int min(int a,int b){
+	if (a<b)
+		return a;
+	return b;	
+}
 
-    return 0;
+int abs(int a){
+	if (a<0)
+		return -a;
+	return a;
+}
+
+int chiphi(int i){
+	if (i==n) return 0;
+	if (f[i] != -1) return f[i];
+	f[i] = chiphi(i+1) + abs(h[i] - h[i+1]);
+	if(i<n-1) 
+		f[i] = min(f[i], chiphi(i+2) + abs(h[i] - h[i+2]));
+	return f[i];
 }
